d0cbe7b5a184815f9c6bec75dad275f020c739c0
KSL <lordharambae@gmail.com>
2018-04-23 15:23:09 +0530

    licensing + tensor update

--- src/network.hpp
+++ src/network.hpp
@@ -20,13 +20,11 @@ public:
 	~network();
 
 	template <class T> void train(const tensor<T>&, const tensor<T>&);
-	template <class T> void train(tensor<T>&&, tensor<T>&&);
-
 	template <class T> void train(const tensor<tensor<T>>&, const tensor<tensor<T>>&);
-	template <class T> void train(tensor<tensor<T>>&&, tensor<tensor<T>>&&);
 
 	template <class T> tensor<T> calculate(const tensor<T>&);
-	template <class T> tensor<T> calculate(tensor<T>&&);
+	template <class T> tensor<T> calculate(const tensor<tensor<T>>&);
+
 
 };
 
--- src/tensor.hpp
+++ src/tensor.hpp
@@ -4,19 +4,52 @@
 
 namespace kraken {
 	
-template <class T>
+template <class T, unsigned long long int S>
 class tensor {
+
+	typedef unsigned long long int size_t;
+	typedef T value_t;
+	typedef tensor<value_t, size_t> tensor_t;
+	typedef value_t& reference;
+	typedef value_t* iterator;
+
+	value_t _buffer[S > 0 ? S : 1];
 
 public:
 
 	tensor();
-	// explicit tensor();
-	tensor(const tensor<T>&);
-	tensor(tensor<T>&&);
-	tensor<T>& operator=(const tensor<T>&);
-	tensor<T>& operator=(tensor<T>&&);
+	explicit tensor();
+	tensor(const tensor_t&);
+	tensor(tensor_t&&);
+	tensor_t& operator=(const tensor_t&);
+	tensor_t& operator=(tensor_t&&);
 	~tensor();
+
+	void fill(const value_t&);
+	void swap(tensor_t&, tensor_t&);
+
+	iterator begin() noexcept;
+	const iterator begin() const noexcept;
+	iterator end() noexcept;
+	const iterator end() const noexcept;
 	
+	constexpr size_t size() const noexcept;
+	constexpr bool empty() const noexcept;
+
+	reference operator[](const size_t&);
+	constexpr const reference operator[](const size_t&) const;
+	reference at(const size_t&);
+	constexpr const reference at(const size_t&) const;
+
+	reference front();
+	constexpr const reference front() const;
+	reference back();
+	constexpr const reference back() const;
+
+	bool operator==(const tensor_t&, const tensor_t&);
+	bool operator!=(const tensor_t&, const tensor_t&);
+
+
 };
 
 }
